32f31b12f85bb0513ebd99a9fbf5dc73e8f61114
lzalten <114494664+lzalten@users.noreply.github.com>
2024-02-11 16:01:55 +0200

    refactor

--- website/urls.py
+++ website/urls.py
@@ -3,15 +3,15 @@ from website.views import HomePageView, SignUpView, MyBooks, AddBook, LibraryVie
     DeleteBook
 
 urlpatterns = [
-    path('', HomePageView.as_view(), name='home'),
-    path('library/', LibraryView.as_view(), name='library'),
-    path('accounts/logout/', LogoutView.as_view(), name='logout'),
+    path('', HomePageView.as_view(), name='home'),  # home page
+    path('library/', LibraryView.as_view(), name='library'),  # all books
+    path('accounts/logout/', LogoutView.as_view(), name='logout'),  # logout
     #path('search_books/', LibraryView.as_view(), name='search_books'),
-    path('search_books/<str:keyword>', SearchBooks.as_view(), name='search_books'),
-    path('delete_book/<int:book_id>', DeleteBook.as_view(), name='delete_book'),
-    path('my_books/', MyBooks.as_view(), name='my_books'),
-    path('add_book/', AddBook.as_view(), name='add_book'),
-    path('single_book/<int:book_id>', SingleBook.as_view(), name='single_book'),
-    path('accounts/signup/', SignUpView.as_view(), name='signup'),
+    path('search_books/<str:keyword>', SearchBooks.as_view(), name='search_books'),  # search books by keyword
+    path('delete_book/<int:book_id>', DeleteBook.as_view(), name='delete_book'),  # delete book by id
+    path('my_books/', MyBooks.as_view(), name='my_books'),  # my books
+    path('add_book/', AddBook.as_view(), name='add_book'),  # adding book
+    path('single_book/<int:book_id>', SingleBook.as_view(), name='single_book'),  #book info
+    path('accounts/signup/', SignUpView.as_view(), name='signup'),  # registration
     path('accounts/', include('django.contrib.auth.urls'), name='auth')
 ]
